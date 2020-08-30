ab621bd97067b08e88c45e48cd728251172653b1
22931 <2293121112@qq.com>
2020-08-30 21:04:19 +0800

    provider服务提供方对暴露接口进行加密

--- myspringcloudconsumer/src/main/java/com/luoamin/controller/ConsumerProductController.java
+++ myspringcloudconsumer/src/main/java/com/luoamin/controller/ConsumerProductController.java
@@ -1,6 +1,9 @@
 package com.luoamin.controller;
 
 import com.luoamin.vo.Product;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -11,7 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/consumer")
 public class ConsumerProductController {
-    // 内网可以直接http://localhost:8080/product/get/调用，外网就需要加密，
+    // 内网可以直接http://localhost:8080/product/get/调用，外网就需要加密，使用sercurity模块，
     // 所以要使用
     public static final String PRODUCT_GET_URL = "http://localhost:8080/product/get/";
     public static final String PRODUCT_LIST_URL="http://localhost:8080/product/list/";
@@ -19,21 +22,26 @@ public class ConsumerProductController {
     @Resource
     private RestTemplate restTemplate;
 
+    @Resource
+    private HttpHeaders httpHeaders;
+
     @RequestMapping("/product/get")
     public Object getProduct(long id) {
-        Product product = restTemplate.getForObject(PRODUCT_GET_URL+id,Product.class);
+        Product product = restTemplate.exchange(PRODUCT_GET_URL + id, HttpMethod.GET,new HttpEntity<Object>(httpHeaders), Product.class).getBody();
         return  product;
     }
 
     @RequestMapping("/product/list")
     public  Object listProduct() {
-        List<Product> list = restTemplate.getForObject(PRODUCT_LIST_URL,List.class);
+        List<Product> list = restTemplate.exchange(PRODUCT_LIST_URL,HttpMethod.GET,
+                new HttpEntity<Object>(httpHeaders), List.class).getBody();
         return  list;
     }
 
     @RequestMapping("/product/add")
     public Object addPorduct(Product product) {
-        Boolean result = restTemplate.postForObject(PRODUCT_ADD_URL,product,Boolean.class);
+        Boolean result = restTemplate.exchange(PRODUCT_ADD_URL, HttpMethod.POST,
+                new HttpEntity<Object>(product,httpHeaders), Boolean.class).getBody();
         return  result;
     }
 }
